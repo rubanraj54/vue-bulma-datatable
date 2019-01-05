fcea55cd4102c06116d8b06084cd4b561853cc8e
Rubanraj <rubanraj54@gmail.com>
2019-01-05 19:55:11 +0100

    showing card title from data

--- src/components/VueBulmaTable.vue
+++ src/components/VueBulmaTable.vue
@@ -3,7 +3,7 @@
         <div class="card">
             <header class="card-header">
                 <p class="card-header-title">
-                    Component
+                    {{card_title}}
                 </p>
             </header>
             <div class="card-content">
